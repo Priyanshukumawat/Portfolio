64638f36a67e1bba72a5b665a86c063faa8e16e7
Priaynshu kumawat <kumawatpriyanshu35@gmail.com>
2024-09-20 17:53:33 +0530

    all code

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Priyanshu Kumawat</h1>
+        <p className={styles.description}>
+          A frontend developer with 2.5 years of experience, specializing in building dynamic and responsive user interfaces using React.js.<br />
+          Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
